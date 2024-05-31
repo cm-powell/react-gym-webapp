567fa3a3390db77b48ebafda208a28ad5a302753
callum powell <141951900+cm-powell@users.noreply.github.com>
2024-06-01 00:05:18 +0100

    Initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Button(props) {
+    const {text} = props
+    return (
+    <button className="px-8 py-4 rounded-md mx-auto border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200">
+    <p>{text}</p>
+    </button>
+  )
+}
